397b7e770f8b3d0633fdf644952a61f060d7a13d
mandoo-su <shk7625@gmail.com>
2021-06-11 23:11:44 +0900

    0.0.0.0 create-react-app, transfer data through props

---
+++ react-tutorial/tic-tac-toe/src/Game.js
@@ -0,0 +1,60 @@
+import React from "react";
+import "./Game.css";
+
+class Square extends React.Component {
+    //button
+    render() {
+        return <button className="square">{this.props.value}</button>;
+    }
+}
+
+class Board extends React.Component {
+    //사각형 9개
+    renderSquare(i) {
+        return <Square value={i} />;
+    }
+
+    render() {
+        const status = "Next player: X";
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    //나중에 수정할 자리 표시자 값을 지님
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Game;
