28addad82054f583807828f426c0b11c3d66142b
mandoo-su <shk7625@gmail.com>
2021-06-12 09:12:48 +0900

    0.0.0.0 add calculateWinner, select return

--- react-tutorial/tic-tac-toe/src/Game.js
+++ react-tutorial/tic-tac-toe/src/Game.js
@@ -4,10 +4,7 @@ import "./Game.css";
 function Square(props) {
     // button, 제어되는 component, Board component 가 제어중
     return (
-        <button
-            className="square"
-            onClick={props.onClick}
-        >
+        <button className="square" onClick={props.onClick}>
             {props.value}
         </button>
     );
@@ -24,11 +21,12 @@ class Board extends React.Component {
     }
 
     handleClick(i) {
-        const squares =
-            this.state.squares.slice();
-        squares[i] = this.state.xIsNest
-            ? "X"
-            : "O";
+        const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            // Winner 가 결정되었거나 squares가 이미 채워져있는 경우
+            return;
+        }
+        squares[i] = this.state.xIsNest ? "X" : "O";
         this.setState({
             squares: squares,
             xIsNest: !this.state.xIsNest,
@@ -39,23 +37,23 @@ class Board extends React.Component {
         return (
             <Square
                 value={this.state.squares[i]}
-                onClick={() =>
-                    this.handleClick(i)
-                }
+                onClick={() => this.handleClick(i)}
             />
         );
     }
 
     render() {
-        const status =
-            "Next player:" +
-            (this.state.xIsNest ? "X" : "O");
-
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = "Winner" + winner;
+        } else {
+            status =
+                "Next player " + (this.state.xIsNest ? "X" : "O");
+        }
         return (
             <div>
-                <div className="status">
-                    {status}
-                </div>
+                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -74,6 +72,30 @@ class Board extends React.Component {
             </div>
         );
     }
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (
+            squares[a] &&
+            squares[a] === squares[b] &&
+            squares[a] === squares[c]
+        ) {
+            return squares[a];
+        }
+    }
+    return null;
 }
 
 class Game extends React.Component {
