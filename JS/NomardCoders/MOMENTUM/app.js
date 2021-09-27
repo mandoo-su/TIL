c3e36c19161f3faebdc6ee059203c12f5ef88159
mandoo-su <shk7625@gmail.com>
2021-09-27 16:37:25 +0900

    #3.7 css in js part two

--- JS/NomardCoders/MOMENTUM/app.js
+++ JS/NomardCoders/MOMENTUM/app.js
@@ -1,36 +1,12 @@
-const title = document.querySelector(".hello h1");
-console.dir(title);
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-    console.log("title was Clicked.");
-    // title.style.color = "blue";
-    const currentTitleColor = title.style.color;
-    let newTitleColor;
-    if (currentTitleColor === "blue") {
-        newTitleColor = "tomato";
+    const clickedClass = "clicked";
+    if (h1.classList.contains(clickedClass)) {
+        h1.classList.remove(clickedClass);
     } else {
-        newTitleColor = "blue";
+        h1.classList.add(clickedClass);
     }
-    title.style.color = newTitleColor;
 }
 
-function handleMouseEnter() {
-    title.innerHTML = "Mouse is here!";
-}
-
-function handleMouseLeave() {
-    title.innerHTML = "Mouse is gone!";
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato";
-}
-
-//title.addEventListener("click", handleTitleClick);
-title.onclick = handleTitleClick;
-// when clicked, conduct function handleTitleClick
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
-// (complete)TODO : when clicked again, change color
-
-window.addEventListener("resize", handleWindowResize);
+h1.addEventListener("click", handleTitleClick);
