192bcd61a3cddf3509ad90aa00c8a89469909aad
mandoo-su <shk7625@gmail.com>
2021-09-27 13:57:15 +0900

    #3.4 events part two

--- JS/NomardCoders/MOMENTUM/app.js
+++ JS/NomardCoders/MOMENTUM/app.js
@@ -1,4 +1,20 @@
 const title = document.querySelector(".hello h1");
 console.dir(title);
 
-title.innerHTML = "Gooo GO!";
+function handleTitleClick() {
+    console.log("title was Clicked.");
+    title.style.color = "blue";
+}
+
+function handleMouseEnter() {
+    title.innerHTML = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+    title.innerHTML = "Mouse is gone!";
+}
+title.addEventListener("click", handleTitleClick);
+// when clicked, conduct function handleTitleClick
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
+// TODO : when clicked again, change color
