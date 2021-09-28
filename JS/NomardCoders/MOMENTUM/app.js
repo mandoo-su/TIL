aa38d4f6b81279d59e452e20fbf6358a09dcaf2a
mandoo-su <shk7625@gmail.com>
2021-09-28 21:55:08 +0900

    #4.6 loading username - make function for intergrating overlapped codes

--- JS/NomardCoders/MOMENTUM/app.js
+++ JS/NomardCoders/MOMENTUM/app.js
@@ -1,28 +1,32 @@
-const HIDDEN = "hidden";
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "userName";
 
 const loginForm = document.querySelector(".login-form");
 const loginInput = loginForm.querySelector("input");
 const greeting = document.querySelector(".greeting");
+const savedUserName = localStorage.getItem(USERNAME_KEY);
 
-const link = document.querySelector("a");
 function onLoginSubmit(event) {
     event.preventDefault();
     // 어떤 event의 기본 행동(브라우저 기본 동작)이든지 발생되지 않도록 막음.
     // 현재 브라우저의 새로고침을 막고 있음.
     console.log(loginInput.value);
     const userName = loginInput.value;
-    localStorage.setItem("userName", userName);
-    loginForm.classList.add(HIDDEN);
-    greeting.classList.remove(HIDDEN);
-    // toggle 을 사용하다가 remove로 변경
-    // toggle의 경우 반대로 돌아가는 경우도 있을 시 사용하는게 나을 듯 함.
+    localStorage.setItem(USERNAME_KEY, userName);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    paintGreetings(userName);
+}
+
+function paintGreetings(userName) {
+    greeting.classList.remove(HIDDEN_CLASSNAME);
     greeting.innerHTML = `Hello, ${userName}`;
 }
 
-function onClickLink(event) {
-    event.preventDefault();
-    console.log(event); // PointerEvent
+if (savedUserName === null) {
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    // hide the form
+    paintGreetings(savedUserName);
 }
-
-loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", onClickLink);
