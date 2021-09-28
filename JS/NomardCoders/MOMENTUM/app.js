ae3a0d444eab89698919b8ffd65e872d689ea600
mandoo-su <shk7625@gmail.com>
2021-09-28 18:09:26 +0900

    #4.2 events - submit, event.preventDefault()

--- JS/NomardCoders/MOMENTUM/app.js
+++ JS/NomardCoders/MOMENTUM/app.js
@@ -1,8 +1,10 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+const loginForm = document.querySelector(".login-form");
+const loginInput = loginForm.querySelector("input");
 
-function handleTitleClick() {
-    const clickedClass = "clicked";
-    h1.classList.toggle(clickedClass);
+function onLoginSubmit(event) {
+    event.preventDefault();
+    // 어떤 event의 기본 행동(브라우저 기본 동작)이든지 발생되지 않도록 막음.
+    // 현재 브라우저의 새로고침을 막고 있음.
+    console.log(loginInput.value);
 }
-
-h1.addEventListener("click", handleTitleClick);
+loginForm.addEventListener("submit", onLoginSubmit);
